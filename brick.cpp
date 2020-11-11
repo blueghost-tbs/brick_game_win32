44e4b3d1dc6d7a7e04a6fe59300a4635c1827b2e
Tibor Lajos Füzi <tibor.fuzi@gmail.com>
2020-11-11 20:25:58 +0100

    version 0.3

--- brick.cpp
+++ brick.cpp
@@ -208,7 +208,7 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM l
                     games[active_game].game_init();
                     break;
                 case IDM_HELP_ABOUT:
-                    MessageBox(NULL, TEXT("Brick Game by Tibor Lajos Füzi\n\nWork-in-progress demo version 0.2"), TEXT("Brick Game"), MB_ICONINFORMATION);
+                    MessageBox(NULL, TEXT("Brick Game by Tibor Lajos Füzi\n\nWork-in-progress demo version 0.3"), TEXT("Brick Game"), MB_ICONINFORMATION);
                     break;
                 case IDM_GAME_TETRIS:
                     change_game(GAME_TETRIS, hwnd);
